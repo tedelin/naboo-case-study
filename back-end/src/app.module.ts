e0f210457cf8b3b906faf61da54b2e8c6a277e91
Arthur <arthur.hamel1@orange.fr>
2023-07-14 15:39:32 +0200

    feat: setup mongo with docker

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => {
+        return {
+          uri: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@${process.env.MONGO_HOST_IP}:${process.env.MONGO_PORT}/${process.env.MONGO_INITDB_DATABASE}`,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
